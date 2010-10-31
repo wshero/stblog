7f693ff94288728c0c3a345beddadc351b672558
yangg.hu <yangg.hu@0569491c-0460-11df-afc1-2de6fd616bef>
2010-10-31 22:52:22 +0000

    修改配置文件中的相关参数，以适应0.2.x的程序架构

--- stblog/config/constants.php
+++ stblog/config/constants.php
@@ -36,6 +36,71 @@ define('FOPEN_READ_WRITE_CREATE',				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
+/*
+|--------------------------------------------------------------------------
+| Stblog Version
+|--------------------------------------------------------------------------
+|
+| Major Version . Minor Version . Bug-fix Updates
+|
+*/
+define('STB_VERSION', '0.2.0');
+define('STB_RELEASE', '20101111');
+
+
+/*
+|--------------------------------------------------------------------------
+| Automatically detect the current server env without dirty your hands!
+|--------------------------------------------------------------------------
+|
+|
+*/
+// Local environment for test or dev purpose
+if(strpos($_SERVER['SERVER_NAME'], 'local.') !== FALSE || strpos($_SERVER['SERVER_NAME'], '.local') !== FALSE || $_SERVER['SERVER_NAME'] == 'localhost' OR $_SERVER['SERVER_NAME'] == '127.0.0.1')
+{
+  define('ENV', 'local');
+}
+// Live environment for production purpose
+else
+{
+  define('ENV', 'live');
+}
+
+/*
+|--------------------------------------------------------------------------
+| 自动检测系统的根目录Web Root
+|--------------------------------------------------------------------------
+|
+| 通过当前URL推测系统安装的Web Root: 可以避免每次在config/config.php中手动定义base_url。
+| 注意：
+|	base_url 和 base_uri 并不相同。
+|	base_url包含完整主机域名或IP地址信息和Web路径，比如http://localhost/~Saturn/stcms/。
+|	base_uri仅包含web地址，比如/~Saturn/stcms/。
+|
+*/
+if(isset($_SERVER['HTTP_HOST']))
+{
+	$base_url = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on' ? 'https' : 'http';
+	$base_url .= '://'. $_SERVER['HTTP_HOST'];
+	$base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+	$base_uri = parse_url($base_url, PHP_URL_PATH);
+	
+	if(substr($base_uri, 0, 1) != '/') $base_uri = '/'.$base_uri;
+	if(substr($base_uri, -1, 1) != '/') $base_uri .= '/';
+}
+else
+{
+	$base_url = 'http://localhost/';
+	$base_uri = '/';
+}
+
+// 将 $base_url 和 $base_uri 两个本地 Web Root 变量赋值给常量备用
+define('BASE_URL', $base_url);
+define('BASE_URI', $base_uri);
+define('APPPATH_URI', BASE_URI . APPPATH);
+
+// 销毁本地变量
+unset($base_url, $base_uri);
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
