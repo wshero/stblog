c58c4737ca34bd16c69ef439a7d3039fb71e5b81
yangg.hu <yangg.hu@0569491c-0460-11df-afc1-2de6fd616bef>
2010-11-02 03:02:10 +0000

    优化入口文件index.php && 将system文件夹移至stblog之下

--- trunk/index.php
+++ trunk/index.php
@@ -68,18 +68,6 @@
 
 /*
  *---------------------------------------------------------------
- * PHP ERROR REPORTING LEVEL
- *---------------------------------------------------------------
- *
- * By default CI runs with error reporting set to ALL.	For security
- * reasons you are encouraged to change this to 0 when your site goes live.
- * For more info visit:	 http://www.php.net/error_reporting
- *
- */
-	error_reporting(E_ALL);
-
-/*
- *---------------------------------------------------------------
  * SYSTEM FOLDER NAME
  *---------------------------------------------------------------
  *
@@ -88,7 +76,7 @@
  * as this file.
  *
  */
-	$system_path = "system";
+	$system_path = "stblog/system";
 
 /*
  *---------------------------------------------------------------
@@ -176,12 +164,6 @@
 	// ensure there's a trailing slash
 	$system_path = rtrim($system_path, '/').'/';
 
-	// Is the system path correct?
-	if ( ! is_dir($system_path))
-	{
-		exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
-	}
-
 /*
  * -------------------------------------------------------------------
  *	Now that we know the path, set the main path constants
@@ -201,22 +183,9 @@
 
 	// Name of the "system folder"
 	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
-
-
+	
 	// The path to the "application" folder
-	if (is_dir($application_folder))
-	{
-		define('APPPATH', $application_folder.'/');
-	}
-	else
-	{
-		if ( ! is_dir(BASEPATH.$application_folder.'/'))
-		{
-			exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
-		}
-
-		define('APPPATH', BASEPATH.$application_folder.'/');
-	}
+	define('APPPATH', $application_folder.'/');
 
 /*
  * --------------------------------------------------------------------
