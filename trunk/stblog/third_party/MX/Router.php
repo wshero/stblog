685a85911c58f6ae2068d4d2db403c7776fa4e67
yangg.hu <yangg.hu@0569491c-0460-11df-afc1-2de6fd616bef>
2010-11-02 06:44:03 +0000

    添加MX Modular Seperation

---
+++ trunk/stblog/third_party/MX/Router.php
@@ -0,0 +1,125 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+/* load the MX core module class */
+require dirname(__FILE__).'/Modules.php';
+
+/**
+ * Modular Extensions - HMVC
+ *
+ * Adapted from the CodeIgniter Core Classes
+ * @link	http://codeigniter.com
+ *
+ * Description:
+ * This library extends the CodeIgniter router class.
+ *
+ * Install this file as application/third_party/MX/Router.php
+ *
+ * @copyright	Copyright (c) Wiredesignz 2010-09-09
+ * @version 	5.3.4
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ **/
+class MX_Router extends CI_Router
+{
+	private $module;
+	
+	public function fetch_module() {
+		return $this->module;
+	}
+	
+	public function _validate_request($segments) {		
+		
+		/* locate module controller */
+		if ($located = $this->locate($segments)) return $located;
+		
+		/* use a default 404 controller */
+		if (isset($this->routes['404_override']) AND $segments = explode('/', $this->routes['404_override'])) {
+			if ($located = $this->locate($segments)) return $located;
+		}
+		
+		/* no controller found */
+		show_404();
+	}
+	
+	/** Locate the controller **/
+	public function locate($segments) {		
+		
+		$this->module = '';
+		$this->directory = '';
+		
+		/* use module route if available */
+		if (isset($segments[0]) AND $routes = Modules::parse_routes($segments[0], implode('/', $segments))) {
+			$segments = $routes;
+		}
+	
+		/* get the segments array elements */
+		list($module, $directory, $controller) = array_pad($segments, 3, NULL);
+
+		foreach (Modules::$locations as $location => $offset) {
+		
+			/* module exists? */
+			if (is_dir($source = $location.$module.'/controllers/')) {
+				
+				$this->module = $module;
+				$this->directory = $offset.$module.'/controllers/';
+				
+				/* module sub-controller exists? */
+				if($directory AND is_file($source.$directory.EXT)) {
+					return array_slice($segments, 1);
+				}
+					
+				/* module sub-directory exists? */
+				if($directory AND is_dir($module_subdir = $source.$directory.'/')) {
+							
+					$this->directory .= $directory.'/';
+
+					/* module sub-directory controller exists? */
+					if(is_file($module_subdir.$directory.EXT)) {
+						return array_slice($segments, 1);
+					}
+				
+					/* module sub-directory sub-controller exists? */
+					if($controller AND is_file($module_subdir.$controller.EXT))	{
+						return array_slice($segments, 2);
+					}
+				}
+			
+				/* module controller exists? */			
+				if(is_file($source.$module.EXT)) {
+					return $segments;
+				}
+			}
+		}
+		
+		/* application controller exists? */			
+		if(is_file(APPPATH.'controllers/'.$module.EXT)) {
+			return $segments;
+		}
+		
+		/* application sub-directory controller exists? */
+		if(is_file(APPPATH.'controllers/'.$module.'/'.$directory.EXT)) {
+			$this->directory = $module.'/';
+			return array_slice($segments, 1);
+		}
+	}
+	
+	public function set_class($class) {
+		$this->class = $class;
+	}
+}
